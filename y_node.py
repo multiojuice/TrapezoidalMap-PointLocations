c4720db03df1a04060ff91f80877d4acaec4ab4a
Collin Tod <squablesquable@gmail.com>
2021-11-02 20:28:50 -0400

    add locate() to y_node

--- y_node.py
+++ y_node.py
@@ -1,11 +1,27 @@
 class YNode:
     def __init__(self, segment, up, down):
         self.segment = segment
+
+        # Do calculations needed later for testing if points are above or below
+        # this line segment
+        (x1, y1), (x2, y2) = self.segment
+        dx = x2 - x1
+        dy = y2 - y1
+        self.slope = dy / dx
+        self.y_intercept = y1 - slope * x1
+
         self.up = up
         self.down = down
 
     def locate(self, point):
         x, y = point
-        # TODO calculate if y > f(x) where f is the function of the line
-        # TODO 
-        pass
+
+        # The y value of the line segment at the point's x
+        #   y = mx + b
+        seg_y_at_x = self.slope * x + self.y_intercept
+
+        if seg_y_at_x < y:
+            return up.locate(point)
+        else:
+            return down.locate(point)
+
