b1a76126199a63982c82e9b1d770d8875d97391e
Daria Leigh Chaplin <dxc4643@cs.rit.edu>
2021-11-02 20:14:17 -0400

    Added input parsing

--- trap.py
+++ trap.py
@@ -1,7 +1,27 @@
+import sys
 import numpy as np
+
 """
-Construction of TrapezoidaL Map
+Construction of Trapezoidal Map
 Authors: Daria Chaplin, Owen Sullivan, Collin Tod
 """
 
-pass
+def main():
+    fp = open(sys.argv[1])
+    line_segments = []
+    ll_bound, ur_bound = None, None
+
+    # Read input file (bounding box coords and line segments)
+    for idx, line in enumerate(fp):
+        coords = [float(x) for x in line.split(" ")]
+        if idx == 1:
+            ll_bound = (coords[0], coords[1])
+            ur_bound = (coords[2], coords[3])
+        elif idx > 1:
+            line_segments.append((
+                (coords[0], coords[1]),
+                (coords[2], coords[3])))
+
+
+if __name__ == "__main__":
+    main()
