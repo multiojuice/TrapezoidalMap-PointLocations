8bed35bc91b17ca2af8c62f6ef30520a7cd0e5e1
Collin Tod <squablesquable@gmail.com>
2021-11-17 14:12:07 -0500

    some refactoring, might crash

--- leaf.py
+++ leaf.py
@@ -1,29 +1,76 @@
 Point = tuple[float, float]
 Segment = tuple[Point, Point]
 
-# TODO implemtn __str__
-class Trapezoid:
+class Leaf:
+
+    # Adjacent Leaves (Max of 4 in general position) 
+    left_top = None
+    left_bot = None
+    right_top = None
+    right_bot = None
+
     def __init__(self, up, down, right, left):
         self.up = up
         self.down = down
         self.right = right
         self.left = left
-
-    def __eq__(self, other):
-        return self.up == other.up and \
-               self.down == other.down and \
-               self.right == other.right and \
-               self.left == other.left
-
-
-class Leaf:
-
-    def __init__(self, trap : Trapezoid):
-        self.trap = trap
         self.parents = []
 
+
     def __eq__(self, other):
-        return isinstance(other, Leaf) and self.trap == other.trap
+        return isinstance(other, Leaf)
+               self.up == other.up and \
+               self.down == other.down and \
+               self.right == other.right and \
+               self.left == other.left and\
+
+
+    def next_intersecting(self, seg: Segment):
+        """
+        Iff seg intersects self, then this finds the next leaf to the right of
+        self that the segment also intersects
+        """
+
+        assert right_top is not None or right_bot is not None
+
+        # TODO assert that seg actually crosses the right bounds of this trap
+        # TODO actually test that this works
+
+        # If their the same (only one right neighbor) no need to see where segment goes
+        # (Must end up to the right because there are no intersecting lines in g.p.
+        if right_top == right_bot:
+            return right_top
+
+        # Line calculations 
+        (x1, y1), (x2, y2) = seg
+        dy = y2 - y1
+        dx = x2 - x1
+        slope = dy / dx
+        y_intercept = y1 - x1 * slope
+
+        # the x,y of the point that bounds the right hand side of this trap
+        right_x, right_y = self.right
+
+        # The y value of seg at the rightmost point of the trapezoid
+        y_at_bound = right_x * slope + y_intercept
+
+        if y_at_bound < right_y:
+            return self.right_bot
+        else:
+            return self.right_top
+
+
+    def swap_on_left(self, leaf: Leaf):
+        """
+        Replaces the left references to self with leaf
+        """
+        pass
+
+    def swap_on_right(self, leaf: Leaf):
+        """
+        Replaces the right references to self with leaf
+        """
+        pass
 
     def replace_with(self, node):
         """
--- trap_map.py
+++ trap_map.py
@@ -1,4 +1,4 @@
-from leaf import Leaf, Trapezoid # TODO maybe move trapezoid into its own module
+from leaf import Leaf
 from x_node import XNode
 from y_node import YNode
 
@@ -21,16 +21,17 @@ class TrapMap:
         top_wall = (minx, maxy), ur_bound
         bot_wall = ll_bound, (maxx, miny)
 
-        # The trapezoid that is a bounding box around the segments
-        root_trap = Trapezoid(up=top_wall, down=bot_wall, right=ur_bound, left=ll_bound)
-
         # Tree starts out as a single rectangular trapezoid
-        self.root = Leaf(root_trap)
+        self.root = Leaf(up=top_wall, down=bot_wall, right=ur_bound, left=ll_bound)
 
         for segment in segments:
             self.insert_segment(segment)
 
     def locate(self, point: Point) -> Leaf:
+        """
+        Returns the leaf that represents the trapezoidal area that `point`
+        would lie in
+        """
         assert self.root is not None
 
         node = self.root
@@ -72,26 +73,41 @@ class TrapMap:
         assert isinstance(leaf1, Leaf)
         assert isinstance(leaf2, Leaf)
 
-        # Finds the trapezoids where the segment endpoints lie
-        trap1, trap2 = map(lambda l: l.trap, (leaf1, leaf2))
-
         # Check if endpoints end in the same trapezoid or not
         # This is case 2 on the slides
         if leaf1 == leaf2:
 
             # Construct the 4 new trapezoids that are created by this segment
-            top_trap = Trapezoid(up=trap1.up, down=segment, right=p2, left=p1)
-            bot_trap = Trapezoid(up=segment, down=trap1.up, right=p2, left=p1)
-            right_trap = Trapezoid(up=trap1.up, down=trap1.down, right=trap1.right, left=p2)
-            left_trap = Trapezoid(up=trap1.up, down= trap1.down, right=p1, left=trap1.left)
+            top_leaf = Leaf(up=leaf1.up, down=segment, right=p2, left=p1)
+            bot_leaf = Leaf(up=segment, down=leaf1.up, right=p2, left=p1)
+            right_leaf = Leaf(up=leaf1.up, down=leaf1.down, right=leaf1.right, left=p2)
+            left_leaf = Leaf(up=leaf1.up, down= leaf1.down, right=p1, left=leaf1.left)
 
-            # Wrap the trapezoids in leaf nodes
-            # TODO reassess if this is necessary
-            top_leaf = Leaf(top_trap)
-            bot_leaf = Leaf(bot_trap)
-            right_leaf = Leaf(right_trap)
-            left_leaf = Leaf(left_trap)
+            # Set neighbors of the left leaf
+            left_leaf.left_top = leaf1.left_top
+            left_leaf.left_bot = leaf1.left_bot
+            left_leaf.right_top = top_leaf
+            left_leaf.right_bot = bot_leaf
 
+            # Set neighbors of the right leaf
+            right_leaf.left_top = top_leaf
+            right_leaf.left_bot = bot_leaf
+            right_leaf.right_top = leaf1.right_top
+            right_leaf.right_bot = leaf1.right_bot
+
+            # Set neighbors of the top leaf
+            top_leaf.right_top = right_leaf
+            top_leaf.right_bot = right_leaf
+            top_leaf.left_top = left_leaf
+            top_leaf.left_bot = left_leaf
+
+            # Set neighbors of the bot leaf
+            bot_leaf.right_top = right_leaf
+            bot_leaf.right_bot = right_leaf
+            bot_leaf.left_top = left_leaf
+            bot_leaf.left_bot = left_leaf
+
+            # Constructs the new subtree that this leaf turns into
             sub_root = XNode(p1)
             sub_root.attach_left(left_leaf)
 
@@ -106,11 +122,49 @@ class TrapMap:
 
             sub_root.attach_right(other_point_node)
 
+            # If the leaf is root, it doesn't have any parents and thus the
+            # root reference itself should be swapped
             if leaf1 == self.root:
                 self.root = sub_root
             else:
                 leaf1.replace_with(sub_root)
 
         else:
-            pass
-            # TODO hard case
+
+            # TODO maybe rename like this everywhere?
+            leaf_begin = leaf1
+            leaf_end = leaf2
+
+            # TODO split up leaf_begin
+
+            # The leftmost trapezoid of the single endpoint split
+            # Has all the same bounds as leaf_begin except a new right point
+            leaf_leftmost = Leaf(up=leaf_begin.up, down=leaf_begin.down,
+                                 left=leaf_begin.left, right=p1)
+
+            # The top of the single endpoint split
+            leaf_begin_top = Leaf(up=leaf_begin.up, down=segment,
+                                  left=p1, right = leaf_begin.right)
+
+            # The bottom of the single endpoint split
+            leaf_begin_bot = Leaf(up=segment, down=leaf_begin.down,
+                                  left=p1, right = leaf_begin.right)
+
+            # Set up neighbors for these new leaves
+            leaf_leftmost.left_top = leaf_begin.left_top
+            leaf_leftmost.left_bot = leaf_begin.left_bot
+            leaf_leftmost.right_top = leaf_begin_top
+            leaf_leftmost.right_bot = leaf_begin_bot
+
+            # TODO set up the tree structure for these new splits (note that
+            # begin_top and begin_bot may be merged with a trapezoid to the
+            # right
+
+            # TODO iterate over every leaf that seg intersects, splitting them horizontally
+                # TODO figure out if the segment is coming on top of or below the left bounding point. 
+                #      this is important because the new segment will block the
+                #      bounding point on the opposite side, meaning that it
+                #      will be merged with the previous bot/top left leaf to
+                #      this one.
+            # TODO split up leaf_end
+
